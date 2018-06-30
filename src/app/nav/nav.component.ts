46f941eca136ecd440d5fec1d6c79493200c20cf
mrcreatist <ahk.verma102@gmail.com>
2018-06-30 09:00:24 +0530

    added country service

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,44 +3,44 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.scss"]
 })
 export class NavComponent implements OnInit {
-  @HostBinding('class') NavComponentClass = 'app-nav';
+  @HostBinding("class") NavComponentClass = "app-nav";
+
+  readonly COUNTRY_API_URL = `https://restcountries.eu/rest/v2/alpha`;
+  private _COUNTRY_URL_TYPE = `codes`;
 
   posts = [];
   sources = [];
+  sourceCountries = [];
   showNews = false;
 
-  private _search = '';
-  private _source = 'the-verge';
-  private _language = 'en';
-  private _sortBy = 'publishedAt';
+  private _search = "";
+  private _source = "the-verge";
+  private _language = "en";
+  private _sortBy = "publishedAt";
   private _pageSize = 20;
   private _page = 1;
 
-  constructor(
-    private _http: HttpClient,
-    private _newsapi: NewsApiService
-  ) { }
+  constructor(private _http: HttpClient, private _newsapi: NewsApiService) {}
 
   ngOnInit() {
     this.getSourceList();
     this.getPosts();
   }
 
-
   getPosts() {
     this._newsapi.resetrequestParameter();
 
-    this._newsapi.setParam('q', this.getSearch());
-    this._newsapi.setParam('sources', this.getSource());
-    this._newsapi.setParam('language', this.getLanguage());
-    this._newsapi.setParam('sortBy', this.getSortBy());
-    this._newsapi.setParam('pageSize', this.getPageSize());
-    this._newsapi.setParam('page', this.getPage());
+    this._newsapi.setParam("q", this.getSearch());
+    this._newsapi.setParam("sources", this.getSource());
+    this._newsapi.setParam("language", this.getLanguage());
+    this._newsapi.setParam("sortBy", this.getSortBy());
+    this._newsapi.setParam("pageSize", this.getPageSize());
+    this._newsapi.setParam("page", this.getPage());
 
     const url = this._newsapi.requestUrl();
     this._http.get(url).subscribe(
@@ -49,10 +49,10 @@ export class NavComponent implements OnInit {
         this.showNews = true;
       },
       (err: any) => {
-        console.log('GENERATING ERROR REPORT');
-        console.log('Content-type:', this._newsapi.getContentType());
-        console.log('Params', this._newsapi.getParam());
-        console.log('Parameters', this._newsapi.getRequestParameter());
+        console.log("GENERATING ERROR REPORT");
+        console.log("Content-type:", this._newsapi.getContentType());
+        console.log("Params", this._newsapi.getParam());
+        console.log("Parameters", this._newsapi.getRequestParameter());
         console.log(err);
       }
     );
@@ -62,6 +62,9 @@ export class NavComponent implements OnInit {
     this._http.get(this._newsapi.getSourcesList()).subscribe(
       (res: any) => {
         for (const src of res.sources) {
+          if (src.country === "zh") {
+            continue;
+          }
           this.sources.push({
             name: src.name,
             id: src.id,
@@ -69,8 +72,14 @@ export class NavComponent implements OnInit {
             language: src.language,
             country: src.country
           });
+          if (!this.countryExists(src.country)) {
+            this.sourceCountries.push({
+              name: "",
+              code: src.country
+            });
+          }
         }
-        // console.log(this.sources);
+        this.updateCountryNames();
       },
       (error: any) => {}
     );
@@ -124,8 +133,42 @@ export class NavComponent implements OnInit {
   }
 
   showPost(post, index) {
-    return post.title !== null ?
-      ((index > 0) ? !(this.posts[index - 1].title === post.title) : true) :
-      false;
+    return post.title !== null
+      ? index > 0
+        ? !(this.posts[index - 1].title === post.title)
+        : true
+      : false;
+  }
+
+  countryExists(countryCode): boolean {
+    for (const country of this.sourceCountries) {
+      if (country.code === countryCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  updateCountryNames() {
+    let query = '';
+    let url = '';
+    for (let i = 0; i < this.sourceCountries.length; i++) {
+      query +=
+        this.sourceCountries[i].code +
+        (i + 1 === this.sourceCountries.length ? '' : ';');
+    }
+
+    url = this.COUNTRY_API_URL + '?' + this._COUNTRY_URL_TYPE + '=' + query;
+
+    this._http.get(url).subscribe((response: any) => {
+      for (let res = 0; res < response.length; res++) {
+        this.sourceCountries[res].name = response[res].name;
+      }
+    });
+  }
+
+  setCountry(event): void {
+    console.log(event.target.value);
+    // this.getPosts();
   }
 }
