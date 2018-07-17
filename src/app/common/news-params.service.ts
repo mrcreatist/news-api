203824bd06749cc9cdadd0ba7569d2a0bc6a0e2a
mrcreatist <ahk.verma102@gmail.com>
2018-07-18 00:30:09 +0530

    completed functional structure

---
+++ src/app/common/news-params.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from '../../../node_modules/rxjs';
+
+@Injectable()
+export class NewsParamsService {
+
+  initalParams = {
+    search: '',
+    source: 'the-verge',
+    language: 'en',
+    sortBy: 'publishedAt',
+    pageSize: this.pageSize,
+    page: this.page
+  };
+
+  private messageSource = new BehaviorSubject(this.initalParams);
+  newsSource = this.messageSource.asObservable();
+
+  private _pageSize = 10;
+  private _page = 1;
+
+  constructor() { }
+
+  setNewsSource(message: any) {
+    this.messageSource.next(message);
+  }
+
+  public get pageSize(): number {
+    return this._pageSize;
+  }
+  public set pageSize(value: number) {
+    this._pageSize = value;
+  }
+
+  public get page(): number {
+    return this._page;
+  }
+  public set page(value: number) {
+    this._page = value;
+  }
+}
